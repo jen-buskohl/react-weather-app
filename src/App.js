d2c91a4e808f6667ed4880f2827a8495e8e4ddff
jen-buskohl <124739446+jen-buskohl@users.noreply.github.com>
2023-09-28 16:28:33 -0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jen Buskohl and is{" "}
+        <a
+          href="https://github.com/jen-buskohl/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
