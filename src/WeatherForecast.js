71c7b381a1f01610ae11861d1c9cb1b6b66553d2
jen-buskohl <124739446+jen-buskohl@users.noreply.github.com>
2023-10-10 16:00:32 -0700

    Added API integration for forecast info

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,52 +2,20 @@ import React from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast(response) {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(props);
+  }
+
+  let apiKey = "o2972aff83bt1a433411747ae6c9e0f0";
+  let city = props.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="forecast-day">Thu</div>
-          <div className="forecast-icon">
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          </div>
-          <div className="forecast-temp">
-            <span className="forecast-temp-max">19°</span>
-            <span className="forecast-temp-min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="forecast-day">Fri</div>
-          <div className="forecast-icon">
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          </div>
-          <div className="forecast-temp">
-            <span className="forecast-temp-max">19°</span>
-            <span className="forecast-temp-min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="forecast-day">Sat</div>
-          <div className="forecast-icon">
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          </div>
-          <div className="forecast-temp">
-            <span className="forecast-temp-max">19°</span>
-            <span className="forecast-temp-min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="forecast-day">Sun</div>
-          <div className="forecast-icon">
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          </div>
-          <div className="forecast-temp">
-            <span className="forecast-temp-max">19°</span>
-            <span className="forecast-temp-min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="forecast-day">Mon</div>
           <div className="forecast-icon">
             <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
           </div>
